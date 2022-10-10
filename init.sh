6809eeae7df8fae1ab6beb19b87f8b0946a98b57
lbaudet <laurent.baudet@zenika.com>
2022-10-10 17:38:53 +0200

    add other apps

--- init.sh
+++ init.sh
@@ -6,10 +6,11 @@ ssh-keygen -N "" -f /home/$USER/.ssh/id_rsa
 ssh-copy-id $USER@127.0.0.1
 
 sudo apt install ansible -y
+sudo apt install git -y
 
 sudo mkdir -p /etc/ansible
 sudo cp hosts /etc/ansible/hosts
 ansible-inventory --list -y
 
-
-#ansible-playbook  -u $USER main.yaml -K
+git clone https://github.com/baudet-zenika/ubuntu-plc.git
+ansible-playbook  -u $USER ubuntu-plc/ansilble/main.yaml -K
